5a65315c33d6011e68d8864a03723ef5e4db0072
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-07-06 01:00:59 +0300

    pass params to controller

--- src/Route/TelegramWebHookController.php
+++ src/Route/TelegramWebHookController.php
@@ -4,14 +4,33 @@ declare(strict_types=1);
 namespace Sokil\TelegramBot\Route;
 
 use React\Http\Response;
+use Sokil\TelegramBot\Service\TelegramBotClient\TelegramBotClientInterface;
 
 class TelegramWebHookController
 {
+    /**
+     * @var TelegramBotClientInterface
+     */
+    private $telegramBotClient;
+
+    /**
+     * TelegramWebHookController constructor.
+     * @param TelegramBotClientInterface $telegramBotClient
+     */
+    public function __construct(TelegramBotClientInterface $telegramBotClient)
+    {
+        $this->telegramBotClient = $telegramBotClient;
+    }
+
     /**
      * @return Response
      */
     public function handle(): Response
     {
-        return new Response(200, [], 'Hello world');
+        return new Response(
+            200,
+            [],
+            $this->telegramBotClient->getWebHookInfo()->getUrl()
+        );
     }
 }
