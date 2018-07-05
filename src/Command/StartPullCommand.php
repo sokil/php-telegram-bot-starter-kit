82f827e74d5191f4533cec7370ae1d59ef6a8759
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-07-05 22:43:39 +0300

    clear pull

--- src/Command/StartPullCommand.php
+++ src/Command/StartPullCommand.php
@@ -29,11 +29,6 @@ class StartPullCommand extends Command
     private $telegram;
 
     /**
-     * @var RouterInterface
-     */
-    private $router;
-
-    /**
      * @var int
      */
     private $httpServerPort;
@@ -66,39 +61,6 @@ class StartPullCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // build absolute URL to webhook
-        $telegramWebHookUrl = $this->router->generate('telegramWebHook');
-        die($telegramWebHookUrl);
 
-        // Set web hook
-        $result = $this->telegram->setWebhook($this->webHookUrl);
-        if ($result->isOk()) {
-            echo $result->getDescription();
-        } else {
-            $output->writeln(sprintf('<error>%s</error>', 'Unknown error'));
-        }
-
-        // start server
-        $loop = ReactEventLoopFactory::create();
-
-        $server = new ReactHttpServer(function (ServerRequestInterface $request) {
-            return new ReactHttpResponse(
-                200,
-                array('Content-Type' => 'text/plain'),
-                $request->getUri()
-            );
-        });
-
-        $socket = new ReactSocketServer($this->httpServerPort, $loop);
-        $server->listen($socket);
-
-        $output->writeln(sprintf(
-            '<info>Bot listens web hooks at at http://127.0.0.1:%d',
-            $this->httpServerPort
-        ));
-
-        $loop->run();
-
-        return 0;
     }
 }
