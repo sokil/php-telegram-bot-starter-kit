0cf8847ea9fa203d88108cec063b473483853471
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-07-07 00:29:13 +0300

    extract http server

--- src/Command/StartPushCommand.php
+++ src/Command/StartPushCommand.php
@@ -3,19 +3,11 @@ declare(strict_types=1);
 
 namespace Sokil\TelegramBot\Command;
 
-use Psr\Http\Server\RequestHandlerInterface;
+use Sokil\TelegramBot\Service\HttpServer\HttpServer;
 use Sokil\TelegramBot\Service\TelegramBotClient\TelegramBotClientInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use React\EventLoop\Factory as ReactEventLoopFactory;
-use React\Socket\Server as ReactSocketServer;
-use React\Http\Server as ReactHttpServer;
-use React\Http\Response as ReactHttpResponse;
-use Psr\Http\Message\ServerRequestInterface;
-use Symfony\Component\DependencyInjection\ServiceLocator;
-use Symfony\Component\Routing\Exception\MethodNotAllowedException;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 
@@ -40,33 +32,25 @@ class StartPushCommand extends Command
     private $router;
 
     /**
-     * @var ServiceLocator
+     * @var HttpServer
      */
-    private $requestHandlerLocator;
-
-    /**
-     * @var int
-     */
-    private $httpServerPort;
+    private $httpServer;
 
     /**
      * @param TelegramBotClientInterface $telegram
      * @param RouterInterface $router
-     * @param ServiceLocator $requestHandlerLocator
-     * @param int $httpServerPort
+     * @param HttpServer $httpServer
      */
     public function __construct(
         TelegramBotClientInterface $telegram,
         RouterInterface $router,
-        ServiceLocator $requestHandlerLocator,
-        int $httpServerPort
+        HttpServer $httpServer
     ) {
         parent::__construct(null);
 
         $this->telegram = $telegram;
         $this->router = $router;
-        $this->requestHandlerLocator = $requestHandlerLocator;
-        $this->httpServerPort = $httpServerPort;
+        $this->httpServer = $httpServer;
     }
 
     /**
@@ -101,69 +85,7 @@ class StartPushCommand extends Command
             return 1;
         }
 
-        // create HTTP server
-        $server = new ReactHttpServer(function (ServerRequestInterface $request) use ($output) {
-            try {
-                // apply request method to context
-                $routerContext = $this->router->getContext();
-                $routerContext->setMethod($request->getMethod());
-
-                // get route parameters
-                $parameters = $this->router->match($request->getUri()->getPath());
-
-                // handle request
-                if (empty($parameters['_controller'])) {
-                    throw new \Exception(sprintf(sprintf('Request handler for route "%s" not specified', $parameters['_router'])));
-                }
-
-                if (!$this->requestHandlerLocator->has($parameters['_controller'])) {
-                    throw new ResourceNotFoundException(sprintf('Request handler "%" not configured', $parameters['_controller']));
-                }
-
-                /** @var RequestHandlerInterface $requestHandler */
-                $requestHandler = $this->requestHandlerLocator->get($parameters['_controller']);
-
-                $response = $requestHandler->handle($request);
-            } catch (MethodNotAllowedException $e) {
-                $response = new ReactHttpResponse(
-                    400,
-                    array('Content-Type' => 'text/plain'),
-                    'Bad method'
-                );
-            } catch (ResourceNotFoundException $e) {
-                $response = new ReactHttpResponse(
-                    404,
-                    array('Content-Type' => 'text/plain'),
-                    'Not found'
-                );
-            } catch (\Throwable $e) {
-                $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
-                $output->writeln(sprintf('<error>%s</error>', $e->getTraceAsString()));
-
-                $response = new ReactHttpResponse(
-                    500,
-                    array('Content-Type' => 'text/plain'),
-                    'Route not found'
-                );
-            }
-
-            return $response;
-        });
-
-        // build event loop
-        $loop = ReactEventLoopFactory::create();
-
-        // create TCP server
-        $socket = new ReactSocketServer($this->httpServerPort, $loop);
-        $server->listen($socket);
-
-        // run bot
-        $output->writeln(sprintf(
-            '<info>Bot listens web hooks at http://127.0.0.1:%d',
-            $this->httpServerPort
-        ));
-
-        $loop->run();
+        $this->httpServer->run();
 
         return 0;
     }
---
+++ src/Service/HttpServer/HttpServer.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+namespace Sokil\TelegramBot\Service\HttpServer;
+
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Component\Routing\RouterInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use React\EventLoop\Factory as ReactEventLoopFactory;
+use React\Socket\Server as ReactSocketServer;
+use React\Http\Server as ReactHttpServer;
+use React\Http\Response as ReactHttpResponse;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+class HttpServer
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * @var ServiceLocator
+     */
+    private $requestHandlerLocator;
+
+    /**
+     * @var int
+     */
+    private $httpServerPort;
+
+    /**
+     * @param RouterInterface $router
+     * @param ServiceLocator $requestHandlerLocator
+     * @param int $httpServerPort
+     */
+    public function __construct(
+        RouterInterface $router,
+        ServiceLocator $requestHandlerLocator,
+        int $httpServerPort
+    ) {
+        $this->router = $router;
+        $this->requestHandlerLocator = $requestHandlerLocator;
+        $this->httpServerPort = $httpServerPort;
+    }
+
+    /**
+     * Run server
+     */
+    public function run(): void
+    {
+        // create HTTP server
+        $server = new ReactHttpServer(function (ServerRequestInterface $request) {
+            try {
+                // apply request method to context
+                $routerContext = $this->router->getContext();
+                $routerContext->setMethod($request->getMethod());
+
+                // get route parameters
+                $parameters = $this->router->match($request->getUri()->getPath());
+
+                // handle request
+                if (empty($parameters['_controller'])) {
+                    throw new \Exception(sprintf(sprintf('Request handler for route "%s" not specified', $parameters['_router'])));
+                }
+
+                if (!$this->requestHandlerLocator->has($parameters['_controller'])) {
+                    throw new ResourceNotFoundException(sprintf('Request handler "%" not configured', $parameters['_controller']));
+                }
+
+                /** @var RequestHandlerInterface $requestHandler */
+                $requestHandler = $this->requestHandlerLocator->get($parameters['_controller']);
+
+                $response = $requestHandler->handle($request);
+            } catch (MethodNotAllowedException $e) {
+                $response = new ReactHttpResponse(
+                    400,
+                    array('Content-Type' => 'text/plain'),
+                    'Bad method'
+                );
+            } catch (ResourceNotFoundException $e) {
+                $response = new ReactHttpResponse(
+                    404,
+                    array('Content-Type' => 'text/plain'),
+                    'Not found'
+                );
+            } catch (\Throwable $e) {
+                $response = new ReactHttpResponse(
+                    500,
+                    array('Content-Type' => 'text/plain'),
+                    'Route not found'
+                );
+            }
+
+            return $response;
+        });
+
+        // build event loop
+        $loop = ReactEventLoopFactory::create();
+
+        // create TCP server
+        $socket = new ReactSocketServer($this->httpServerPort, $loop);
+        $server->listen($socket);
+
+        // run bot
+        $loop->run();
+    }
+}
